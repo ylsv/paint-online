3af344193904692a4c340c87783faf3ff20ea0c7
Юрий Лисов <yulisov@fil-it.ru>
2022-07-29 20:29:38 +0300

    add toolbar buttons and images

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -3,7 +3,21 @@ import '../styles/toolbar.scss'
 
 const ToolBar = () => {
   return (
-    <div className='toolbar'></div>
+    <div className='toolbar'>
+      <div className="toolbar-section">
+        <button className="toolbar__btn brush"></button>
+        <button className="toolbar__btn rect"></button>
+        <button className="toolbar__btn circle"></button>
+        <button className="toolbar__btn eraser"></button>
+        <button className="toolbar__btn line"></button>
+        <input type="color" />
+      </div>
+      <div className="toolbar-section">
+        <button className="toolbar__btn undo"></button>
+        <button className="toolbar__btn redo"></button>
+        <button className="toolbar__btn save"></button>
+      </div>
+    </div>
   )
 }
 
